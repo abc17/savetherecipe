663e4046fee8d94fa634778538c06f248b6c4792
Anna Kutukova <annakutukova@gmail.com>
2023-08-28 16:12:17 +0300

    complicated logging to Mongo

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import sentry_sdk
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
+import datetime
 
 # Загрузка переменных среды из файла .env
 load_dotenv(dotenv_path=".env")
@@ -52,20 +53,46 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+def log_interaction(event_type, user_id, user_name, event_data):
+    log_entry = {
+        "event_type": event_type,
+        "user_id": user_id,
+        "user_name": user_name,
+        "event_data": event_data,
+        "timestamp": datetime.datetime.now()  # Добавление времени в объект логирования
+    }
+    collection.insert_one(log_entry)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
-    # user = update.effective_user
-    await update.message.reply_text(
-        "Отправьте первую ссылку, чтобы посмотреть, как это работает. \n\nНапример, эту: https://www.instagram.com/reel/Cr0g43KIznu/?igshid=MTc4MmM1YmI2Ng%3D%3D",
-        disable_web_page_preview=True
+    user_id = update.effective_user.id
+    user_name = update.effective_user.username
+
+    reply_text = (
+        "Отправьте первую ссылку, чтобы посмотреть, как это работает. \n\n"
+        "Например, эту: https://www.instagram.com/reel/Cr0g43KIznu/?igshid=MTc4MmM1YmI2Ng%3D%3D"
     )
 
+    await update.message.reply_text(reply_text, disable_web_page_preview=True)
+
+    log_interaction("start", user_id, user_name, {"reply_text": reply_text})
+
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text(
-        "Если что-то не работает, есть вопросы или предложения по работе бота - напишите мне @anna_abc")
+    user_id = update.effective_user.id
+    user_name = update.effective_user.username
+    reply_text = "Если что-то не работает, есть вопросы или предложения по работе бота - напишите мне @anna_abc"
+
+    await update.message.reply_text(reply_text)
+
+    log_entry = {
+        "timestamp": datetime.datetime.now(),
+        "event_type": "help_command",
+        "user_id": user_id,
+        "user_name": user_name,
+        "reply_text": reply_text
+    }
+    collection.insert_one(log_entry)
 
 # async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 #   """Echo the user message."""
@@ -74,6 +101,23 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 async def handle_message(update, context):
     chat_id = update.message.chat_id
     url = update.message.text
+    user_id = update.effective_user.id
+    user_name = update.effective_user.username
+    user_message = update.message.text
+
+    # Получение текущего времени
+    current_time = datetime.datetime.now()
+
+    # Логирование сообщения пользователя
+
+    log_entry = {
+        "timestamp": current_time,
+        "event_type": "user_message",
+        "user_id": user_id,
+        "user_name": user_name,
+        "message_text": user_message,
+    }
+    collection.insert_one(log_entry)
 
     # Регулярное выражение для ссылок с параметром igshid и без него
     pattern_with_igshid = r'/([^/?]+)/\??igshid='
@@ -87,7 +131,7 @@ async def handle_message(update, context):
     elif shortcode_without_igshid:
         shortcode = shortcode_without_igshid.group(1)
     else:
-        await update.message.reply_text("Пожалуйста, отправьте ссылку пост или рилс в Instagram .")
+        await update.message.reply_text("Пожалуйста, отправьте ссылку пост или рилс в Instagram.")
         return
 
     try:
@@ -109,38 +153,49 @@ async def handle_message(update, context):
         text_parts = [post_text_cleaned[i:i + max_message_length] for i in
                       range(0, len(post_text_cleaned), max_message_length)]
 
+        # Подготовка переменной для логирования успешного ответа бота
+        response_text = ""
+
         # Проверка, нужно ли разбивать сообщение
         if len(text_parts) == 1:
             # Подготовка подписи с текстом и изображением
             post_text_with_image = "{}\n\n---\nИсточник: {}".format(text_parts[0], url)
             await update.message.reply_photo(photo=BytesIO(image_data), caption=post_text_with_image)
+            response_text = post_text_with_image
         else:
             # Отправка текста с изображением в первом сообщении
             post_text_with_image = "{}".format(text_parts[0])
             await update.message.reply_photo(photo=BytesIO(image_data), caption=post_text_with_image)
+            response_text = post_text_with_image
 
             # Отправка оставшегося текста (без изображения) с отключением предварительного просмотра ссылок
             for text_part in text_parts[1:]:
                 post_text_with_source = "{}\n\n-------\nИсточник: {}".format(text_part, url)
                 await update.message.reply_text(post_text_with_source, disable_web_page_preview=True)
+                response_text += "\n\n" + post_text_with_source
+
+        # Логирование успешного ответа бота
+
+        log_entry = {
+            "timestamp": current_time,
+            "event_type": "bot_response",
+            "user_id": user_id,
+            "user_name": user_name,
+            "response_text": response_text
+        }
+        collection.insert_one(log_entry)
 
         # Удаление исходного сообщения пользователя
         await context.bot.delete_message(chat_id=chat_id, message_id=update.message.message_id)
 
-        # Создание объекта для логирования
-        log_entry = {
-            "user_id": update.effective_user.id,
-            "user_name": update.effective_user.username,
-            "message": update.message.text,
-            "post_text": post_text_cleaned
-        }
-
-        # Сохранение данных в MongoDB
-        collection.insert_one(log_entry)
-
     except Exception as e:
+        # Логирование ошибки
+        log_interaction("error", user_id, user_name, {"error_message": str(e)})
+
         logging.exception("Произошла ошибка при обработке запроса:")
-        await update.message.reply_text("Произошла ошибка. Проверьте, что ссылка ведет на пост или рилс в Instagram. К сожалению, ссылки из других сервисов бот пока не распознает.")
+        await update.message.reply_text(
+            "Произошла ошибка. Проверьте, что ссылка ведет на пост или рилс в Instagram. К сожалению, ссылки из других сервисов бот пока не распознает.")
+
 
 def main() -> None:
     """Start the bot."""
